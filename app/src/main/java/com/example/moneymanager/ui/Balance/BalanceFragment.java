35ba4ec93fe43655bc58ca15d9230f29b2d52b03
Billy <yuhanglo7777@gmail.com>
2022-04-05 00:52:00 +0800

    spinner

--- app/src/main/java/com/example/moneymanager/ui/Balance/BalanceFragment.java
+++ app/src/main/java/com/example/moneymanager/ui/Balance/BalanceFragment.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.moneymanager.R;
 import com.example.moneymanager.databinding.FragmentBalanceBinding;
+
+import java.lang.reflect.Array;
+
 public class BalanceFragment extends Fragment {
 
     private FragmentBalanceBinding binding;
@@ -26,6 +34,12 @@ public class BalanceFragment extends Fragment {
         final TextView textView = binding.textDashboard;
         balanceViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+        Spinner spinner = (Spinner)root.findViewById(R.id.spinner1);
+        final String[] Type = {"衣", "食", "住", "行", "玩"};
+        ArrayAdapter<String> lunchList = new ArrayAdapter<>(BalanceFragment.this,
+                android.R.layout.simple_spinner_dropdown_item,Type);spinner.setAdapter(lunchList);
+
+
     }
 
     @Override
