f9d3b857147941df2e7436f1bdfa3ef2cf7fbfed
BenHo <benho223@gmail.com>
2022-04-06 23:52:25 +0800

    bottom menu bar color icon

--- app/src/main/java/com/example/moneymanager/MainActivity.java
+++ app/src/main/java/com/example/moneymanager/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setItemIconTintList(null);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
