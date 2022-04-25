2b9327fad79c6f5763b5d94a6cd7db8135d4d66f
BenHo <benho223@gmail.com>
2022-04-26 03:28:00 +0800

    SQLite Database of Expense

--- app/src/main/java/com/example/moneymanager/database/DatabaseHelper.java
+++ app/src/main/java/com/example/moneymanager/database/DatabaseHelper.java
@@ -1,9 +1,12 @@
 package com.example.moneymanager.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -12,20 +15,54 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DB_NAME = "MoneyManager.db";
     public static final int DB_VERSION = 1;
 
-    public DatabaseHelper(Context context) {
+    private static final String TABLE_NAME = "Expense";
+    private static final String COL_ID = "id";
+    private static final String COL_DATE = "date";
+    private static final String COL_NOTE = "note";
+    private static final String COL_EXPENSE = "ex_category";
+    private static final String COL_AMOUNT = "amount";
+
+
+
+    public DatabaseHelper(@Nullable Context context) {
         super(context, DB_NAME, null, DB_VERSION);
+        this.context =context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(ExCategoryTable.SQL_CREATE);
+        String exQuery = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
+                "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL_DATE + " DATE , " +
+                COL_NOTE + " TEXT , " +
+                COL_EXPENSE + " TEXT ," +
+                COL_AMOUNT + " FLOAT ) ;";
+        db.execSQL(exQuery);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL(ExCategoryTable.SQL_DELETE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
+
+    public void addData(String date, String note, String ex_category, float amount){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COL_DATE, date);
+        cv.put(COL_NOTE, note);
+        cv.put(COL_EXPENSE, ex_category);
+        cv.put(COL_AMOUNT, amount);
+        long result = db.insert(TABLE_NAME, null, cv);
+        if (result == -1 ){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }else
+            Toast.makeText(context, "Save successfully!", Toast.LENGTH_SHORT).show();
+
+    }
+
 
 
 }
