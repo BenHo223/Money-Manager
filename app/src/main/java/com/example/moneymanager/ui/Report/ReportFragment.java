b51dc778f469587d07b0a310937b8f6eb0fe7b53
Hanson <laihonsan.c@gmail.com>
2022-04-29 01:32:57 +0800

    have ? bug

--- app/src/main/java/com/example/moneymanager/ui/Report/ReportFragment.java
+++ app/src/main/java/com/example/moneymanager/ui/Report/ReportFragment.java
@@ -1,87 +1,6 @@
 package com.example.moneymanager.ui.Report;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import com.example.moneymanager.R;
-import com.example.moneymanager.database.DatabaseHelper;
-import com.example.moneymanager.databinding.FragmentReportBinding;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
-
-import java.util.ArrayList;
-
-public class ReportFragment extends Fragment {
-    DatabaseHelper databaseHelper;
-    SQLiteDatabase sqLiteDatabase;
-
-    PieDataSet pieDataSet =new PieDataSet(null,null);
-
-    private FragmentReportBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        ReportViewModel reportViewModel =
-                new ViewModelProvider(this).get(ReportViewModel.class);
-
-        binding = FragmentReportBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        PieChart pieChart = root.findViewById(R.id.pieChart);
-        databaseHelper = new DatabaseHelper(getActivity());
-        sqLiteDatabase = databaseHelper.getWritableDatabase();
- /*
-        pieDataSet.setValues(getDataValues());
-        pieDataSet.setLabel("Description");
-        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        pieDataSet.setValueTextColor(Color.BLACK);
-        pieDataSet.setValueTextSize(10f);
-
-        PieData pieData = new PieData(pieDataSet);
-
-        pieChart.setData(pieData);
-        pieChart.getDescription().setEnabled(false);
-        pieChart.setCenterText("desc Pie");
-        pieChart.animate();
-
-        pieDataSet.setFormLineWidth(4);
-*/
-        return root;
-    }
-  /*  private ArrayList<PieEntry> getDataValues(){
-        ArrayList<PieEntry> dataValues = new ArrayList<>();
-        Cursor cursor = databaseHelper.getValues();
-
-        for (int i=0;i<cursor.getCount();i++){
-            cursor.moveToNext();
-            dataValues.add(new PieEntry(cursor.getFloat(0),String.valueOf(cursor.getString(1))));
-        }
-        return dataValues;
-    }
-*/
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-}
-
-
-/*
-// Import the required libraries
 import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -89,66 +8,106 @@ import android.widget.TextView;
 import org.eazegraph.lib.charts.PieChart;
 import org.eazegraph.lib.models.PieModel;
 
-public class MainActivity
+public class ReportFragment
         extends AppCompatActivity {
 
     // Create the object of TextView
     // and PieChart class
-    TextView tvR, tvPython, tvCPP, tvJava;
-    PieChart pieChart;
+    TextView tvClothing, tvFood, tvLiving, tvTransport, tvSalary, tvInvestment, tvBonus, tvOthers;
+    org.eazegraph.lib.charts.PieChart pieChartIn, pieChartEx;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Link those objects with their
         // respective id's that
         // we have given in .XML file
-        tvR = findViewById(R.id.tvR);
-        tvPython = findViewById(R.id.tvPython);
-        tvCPP = findViewById(R.id.tvCPP);
-        tvJava = findViewById(R.id.tvJava);
-        pieChart = findViewById(R.id.piechart);
+        tvClothing = findViewById(R.id.tvClothing);
+        tvFood = findViewById(R.id.tvFood);
+        tvLiving = findViewById(R.id.tvLiving);
+        tvTransport = findViewById(R.id.tvTransport);
+        pieChartIn = findViewById(R.id.piechartin);
+
+        // Creating a method setData()
+        // to set the text in text view and pie chart
+
+
+        tvSalary = findViewById(R.id.tvSalary);
+        tvInvestment = findViewById(R.id.tvInvestment);
+        tvBonus = findViewById(R.id.tvBonus);
+        tvOthers = findViewById(R.id.tvOthers);
+        pieChartEx = findViewById(R.id.piechartex);
 
         // Creating a method setData()
         // to set the text in text view and pie chart
         setData();
     }
 
-    private void setData()
-    {
+    private void setData() {
 
         // Set the percentage of language used
-        tvR.setText(Integer.toString(40));
-        tvPython.setText(Integer.toString(30));
-        tvCPP.setText(Integer.toString(5));
-        tvJava.setText(Integer.toString(25));
+        tvClothing.setText(Integer.toString(25));
+        tvFood.setText(Integer.toString(25));
+        tvLiving.setText(Integer.toString(25));
+        tvTransport.setText(Integer.toString(25));
 
         // Set the data and color to the pie chart
-        pieChart.addPieSlice(
+        pieChartIn.addPieSlice(
                 new PieModel(
-                        "R",
-                        Integer.parseInt(tvR.getText().toString()),
+                        "Clothing",
+                        Integer.parseInt(tvClothing.getText().toString()),
                         Color.parseColor("#FFA726")));
-        pieChart.addPieSlice(
+        pieChartIn.addPieSlice(
                 new PieModel(
-                        "Python",
-                        Integer.parseInt(tvPython.getText().toString()),
+                        "Food",
+                        Integer.parseInt(tvFood.getText().toString()),
                         Color.parseColor("#66BB6A")));
-        pieChart.addPieSlice(
+        pieChartIn.addPieSlice(
                 new PieModel(
-                        "C++",
-                        Integer.parseInt(tvCPP.getText().toString()),
+                        "Living",
+                        Integer.parseInt(tvLiving.getText().toString()),
                         Color.parseColor("#EF5350")));
-        pieChart.addPieSlice(
+        pieChartIn.addPieSlice(
                 new PieModel(
-                        "Java",
-                        Integer.parseInt(tvJava.getText().toString()),
+                        "Transport",
+                        Integer.parseInt(tvTransport.getText().toString()),
+                        Color.parseColor("#29B6F6")));
+
+
+
+        // Set the percentage of language used
+       tvSalary.setText(Integer.toString(25));
+        tvInvestment.setText(Integer.toString(25));
+        tvBonus.setText(Integer.toString(25));
+        tvOthers.setText(Integer.toString(25));
+
+        // Set the data and color to the pie chart
+        pieChartEx.addPieSlice(
+                new PieModel(
+                        "Salary",
+                        Integer.parseInt(tvSalary.getText().toString()),
+                        Color.parseColor("#FFA726")));
+        pieChartEx.addPieSlice(
+                new PieModel(
+                        "Investment",
+                        Integer.parseInt(tvInvestment.getText().toString()),
+                        Color.parseColor("#66BB6A")));
+        pieChartEx.addPieSlice(
+                new PieModel(
+                        "Bonus",
+                        Integer.parseInt(tvBonus.getText().toString()),
+                        Color.parseColor("#EF5350")));
+        pieChartEx.addPieSlice(
+                new PieModel(
+                        "Others",
+                        Integer.parseInt(tvOthers.getText().toString()),
                         Color.parseColor("#29B6F6")));
 
         // To animate the pie chart
-        pieChart.startAnimation();
+        pieChartIn.startAnimation();
+        pieChartEx.startAnimation();
+
     }
-}*/
+}
